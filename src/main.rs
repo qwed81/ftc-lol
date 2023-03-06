0fe502a17c9ae2d7d936571d222e59e1f239a6dd
Joshua Cox <jcox.12.2002@gmail.com>
2023-03-05 18:37:55 -0800

    added automatic on process launch

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use std::path::Path;
+use std::time::Duration;
 
 use skins::patch_loader::{Loader, load};
 
+enum Input {
+    Pid, Name, Constant
+}
+
 fn main() {
+    
+    let input = Input::Constant;
+    let lol_path = "C:\\Riot Games\\League of Legends\\Game\\League of Legends.exe";
+
+    let loader = match input {
+        Input::Pid => input_pid(),
+        Input::Name => input_name(),
+        Input::Constant => Loader::wait_spawn(lol_path.as_bytes(), Duration::from_secs(10_000)).unwrap()
+    };
+
+    let file_path = Path::new("patch");
+    // std::thread::sleep(Duration::from_millis(3000));
+    load::load_patch(file_path, loader).unwrap();
+    println!("done loading!");
+}
+
+fn input_name() -> Loader {
+    println!("input file name");
+    let mut name = String::new();
+    std::io::stdin().read_line(&mut name).unwrap();
+    let name = name.trim().as_bytes();
+    Loader::wait_spawn(name, Duration::from_secs(60_000)).unwrap()
+}
+
+fn input_pid() -> Loader {
     println!("input pid: ");
     let mut pid = String::new();
     std::io::stdin().read_line(&mut pid).unwrap();
     let pid = pid.trim().parse::<u32>().unwrap();
-
-    let loader = Loader::from_pid(pid).unwrap();
-
-    let file_path = Path::new("patch");
-    load::load_patch(file_path, loader).unwrap();
-
-    println!("done loading!");
+    Loader::from_pid(pid).unwrap()
 }
--- src/patch_loader/windows_loader.rs
+++ src/patch_loader/windows_loader.rs
@@ -1,21 +1,28 @@
+use winapi::um::handleapi;
 use winapi::um::memoryapi;
 use winapi::um::processthreadsapi;
 use winapi::um::errhandlingapi;
 use winapi::um::tlhelp32::TH32CS_SNAPTHREAD;
 use winapi::um::tlhelp32::THREADENTRY32;
+use winapi::um::psapi;
 use winapi::um::winbase;
 use winapi::um::tlhelp32;
 use winapi::um::winnt::THREAD_ALL_ACCESS;
 use winapi::um::winnt::WOW64_CONTEXT;
 use winapi::um::winnt::WOW64_CONTEXT_FULL;
 use winapi::um::winnt::{
-    PROCESS_VM_OPERATION, PROCESS_VM_READ, PROCESS_VM_WRITE, PROCESS_CREATE_THREAD, PROCESS_QUERY_INFORMATION,
-    MEM_RESERVE, MEM_COMMIT, PAGE_NOACCESS, PAGE_EXECUTE_READWRITE, PAGE_READONLY, PAGE_READWRITE, PAGE_EXECUTE_READ
+    PROCESS_VM_OPERATION, PROCESS_VM_READ, PROCESS_VM_WRITE, PROCESS_QUERY_INFORMATION, MEM_RESERVE,
+    MEM_COMMIT, PAGE_NOACCESS, PAGE_EXECUTE_READWRITE, PAGE_READONLY, PAGE_READWRITE, PAGE_EXECUTE_READ
 };
 
 use winapi::ctypes::c_void;
+use std::mem::MaybeUninit;
 use std::ptr;
 use std::mem;
+use std::thread;
+use std::time::Duration;
+use std::collections::HashSet;
+use std::time::Instant;
 
 use super::{ExPtr, ExLen, MemProt};
 
@@ -33,10 +40,14 @@ struct AlignedContext(WOW64_CONTEXT);
 struct PatchContext {
     _context: u32,
     ret_addr: ExPtr,
-    restore_eax: u32,
     restore_esp: u32,
     restore_ebp: u32
 }
+
+const MAX_PROCESS_FILE_NAME_LEN: usize = 1_000;
+const MAX_PROCESS_ITER: usize = 10_000;
+const PROCESS_POLL_DURATION: Duration = Duration::from_millis(200);
+const REQUIRED_PROCESS_PERMS: u32 = PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION;
 
 impl WindowsLoader {
 
@@ -46,14 +57,88 @@ impl WindowsLoader {
         base + offset
     }
 
-    pub fn from_pid(pid: u32) -> Result<Self, ()> {
-        // get the pid
-        let create_thread_perms = PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION;
-        let mem_perms = PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE;
-        let perms = create_thread_perms | mem_perms;
+    // returns the handle of the process if it has that name, otherwise it returns None
+    fn process_is_named(pid: u32, expected_file_name: &[u8]) -> Option<*mut c_void> {
+        assert!(expected_file_name.len() <= MAX_PROCESS_FILE_NAME_LEN);
+        assert!(expected_file_name.len() > 0);
+        
+        let process_handle = unsafe {
+            processthreadsapi::OpenProcess(REQUIRED_PROCESS_PERMS, 0, pid)
+        };
 
+        if process_handle.is_null() {
+            return None;
+        }
+        
+        let file_name = unsafe {
+            let mut name_buf: MaybeUninit<[u8; MAX_PROCESS_FILE_NAME_LEN]> = MaybeUninit::uninit();
+            let name_buf_ptr = name_buf.assume_init_mut().as_mut_ptr() as *mut i8;
+            let amt_copied = psapi::GetModuleFileNameExA(process_handle, ptr::null_mut(), name_buf_ptr,
+                MAX_PROCESS_FILE_NAME_LEN as u32) as usize;
+            &name_buf.assume_init()[0..amt_copied]
+        };
+
+        // println!("new process path queried: {}", String::from_utf8_lossy(file_name));
+        if file_name == expected_file_name { 
+            Some(process_handle) 
+        } else {
+            // not much to do if this fails, just leak if its still open ig
+            unsafe { handleapi::CloseHandle(process_handle) };
+            None 
+        }
+    }
+
+    pub fn wait_spawn(process_file_name: &[u8], max_wait: Duration) -> Result<WindowsLoader, ()> {
+        let start_time = Instant::now();
+
+        let mut buffer = [0; MAX_PROCESS_ITER];
+        let mut amt_returned = 0;
+
+        let mut valid_pids = HashSet::new();
+        let mut swap_set = HashSet::new();
+        println!("waiting for process: \"{}\"", String::from_utf8_lossy(process_file_name));
+
+        let (pid, handle) = 'outer: loop {
+            let result = unsafe {
+                psapi::EnumProcesses(buffer.as_mut_ptr(), buffer.len() as u32, &mut amt_returned)
+            };
+
+            if result == 0 {
+                let err = unsafe { errhandlingapi::GetLastError() };
+                println!("could not enumerate processes, error: {}", err);
+                return Err(());
+            }
+
+            for i in 0..(amt_returned as usize) {
+                let pid = buffer[i];
+                swap_set.insert(pid);
+
+                // it has already been looked up, and does not have that name
+                if valid_pids.contains(&pid) {
+                    continue;
+                }
+
+                if let Some(handle) = WindowsLoader::process_is_named(pid, process_file_name) {
+                    break 'outer (pid, handle);
+                }
+            }
+            
+            valid_pids.clear();
+            mem::swap(&mut valid_pids, &mut swap_set);
+
+            if Instant::now() > start_time + max_wait {
+                return Err(())
+            }
+
+            thread::sleep(PROCESS_POLL_DURATION);
+        };
+
+        Ok(WindowsLoader { pid, h_proc: handle, reserved: None })
+    }
+
+    pub fn from_pid(pid: u32) -> Result<WindowsLoader, ()> {
         let h_proc = unsafe {
-            processthreadsapi::OpenProcess(perms, 0, pid)
+            processthreadsapi::OpenProcess(REQUIRED_PROCESS_PERMS, 0, pid)
         };
 
         if h_proc.is_null() {
@@ -165,6 +250,33 @@ impl WindowsLoader {
 
         Ok(())
     }
+
+    fn suspend_thread(thread_handle: *mut c_void) -> Result<(), ()> {
+        let suspend_result = unsafe {
+            processthreadsapi::SuspendThread(thread_handle)
+        };
+        if suspend_result == 0xFFFFFFFF {
+            let err = unsafe { errhandlingapi::GetLastError() };
+            println!("thread could not suspend, error is: {}", err);
+            return Err(());
+        }  
+
+        Ok(())
+    }
+
+    fn resume_thread(thread_handle: *mut c_void) -> Result<(), ()> {
+        let resume_result = unsafe {
+            processthreadsapi::ResumeThread(thread_handle)
+        };
+
+        if resume_result == 0xFFFFFFFF {
+            let err = unsafe { errhandlingapi::GetLastError() };
+            println!("thread could not resume, error is: {}", err);
+            return Err(());
+        }  
+
+        Ok(())
+    }
     
     fn open_victim_thread(&self) -> Result<*mut c_void, ()> {
         let snapshot = unsafe {
@@ -227,15 +339,13 @@ impl WindowsLoader {
         let patch_context = PatchContext {
             _context: 0,
             ret_addr: context.0.Eip,
-            restore_eax: context.0.Eax,
             restore_ebp: context.0.Ebp,
             restore_esp: context.0.Esp
         };
 
         println!("return to addr: {:x}", patch_context.ret_addr);
-        println!("restore eax: {:?}", patch_context.restore_eax);
 
-        let context_buf = unsafe { mem::transmute::<PatchContext, [u8; 20]>(patch_context) };
+        let context_buf = unsafe { mem::transmute::<PatchContext, [u8; 16]>(patch_context) };
         self.mem_write_direct(context_addr, &context_buf)?;
 
         const STACK_ADDR: u32 = 0x4500_0000;
@@ -276,46 +386,19 @@ impl WindowsLoader {
         println!("initializing patch");
 
         let thread_handle = self.open_victim_thread()?;
-        suspend_thread(thread_handle)?;
+        WindowsLoader::suspend_thread(thread_handle)?;
 
         let start_addr = self.get_offset_ptr(start_proc_offset);
         let context_addr = self.get_offset_ptr(context_offset);
         self.change_thread_state(thread_handle, start_addr, context_addr)?;
 
-        resume_thread(thread_handle)?;
+        WindowsLoader::resume_thread(thread_handle)?;
 
         Ok(())
     }
 
 }
 
-
-fn suspend_thread(thread_handle: *mut c_void) -> Result<(), ()> {
-    let suspend_result = unsafe {
-        processthreadsapi::SuspendThread(thread_handle)
-    };
-    if suspend_result == 0xFFFFFFFF {
-        let err = unsafe { errhandlingapi::GetLastError() };
-        println!("thread could not suspend, error is: {}", err);
-        return Err(());
-    }  
-
-    Ok(())
-}
-
-fn resume_thread(thread_handle: *mut c_void) -> Result<(), ()> {
-    let resume_result = unsafe {
-        processthreadsapi::ResumeThread(thread_handle)
-    };
-
-    if resume_result == 0xFFFFFFFF {
-        let err = unsafe { errhandlingapi::GetLastError() };
-        println!("thread could not resume, error is: {}", err);
-        return Err(());
-    }  
-
-    Ok(())
-}
 
    /*
 fn create_rel_jmp(instruction_pos: ExPtr, jump_to_pos: ExPtr) -> [u8; 6] {
@@ -331,74 +414,75 @@ fn create_rel_jmp(instruction_pos: ExPtr, jump_to_pos: ExPtr) -> [u8; 6] {
     
     instruction
 }
-    fn start_remote_thread(&mut self, start_addr: ExPtr) -> Result<(), ()> {
-        println!("thread starting at addr: {:x?}", start_addr);
-        let start_fn = unsafe {
-            mem::transmute::<usize, Option<unsafe extern "system" fn (*mut c_void) -> u32>>(0x03D5_0000)
-        };
-        
-        let mut thread_id = 0;
-        const START_PAUSED: u32 = 4;
-        let thread_handle = unsafe {
-            processthreadsapi::CreateRemoteThread(self.h_proc, ptr::null_mut(), 0x2000, 
-                start_fn, ptr::null_mut(), START_PAUSED, &mut thread_id)
-        };
 
-        if thread_handle.is_null() {
-            let err = unsafe { errhandlingapi::GetLastError() }; 
-            println!("could not create thread, error is: {}", err);
-            return Err(());
-        }
+fn start_remote_thread(&mut self, start_addr: ExPtr) -> Result<(), ()> {
+    println!("thread starting at addr: {:x?}", start_addr);
+    let start_fn = unsafe {
+        mem::transmute::<usize, Option<unsafe extern "system" fn (*mut c_void) -> u32>>(0x03D5_0000)
+    };
+    
+    let mut thread_id = 0;
+    const START_PAUSED: u32 = 4;
+    let thread_handle = unsafe {
+        processthreadsapi::CreateRemoteThread(self.h_proc, ptr::null_mut(), 0x2000, 
+            start_fn, ptr::null_mut(), START_PAUSED, &mut thread_id)
+    };
 
-        println!("created thread, id is: {}", thread_id);
-
-        let mut context: AlignedContext = unsafe { mem::zeroed() };
-        context.0.ContextFlags = WOW64_CONTEXT_FULL;
-        let context_result = unsafe {
-            winbase::Wow64GetThreadContext(thread_handle, &mut context.0)
-        };
-        
-        if context_result == 0 {
-            let err = unsafe { errhandlingapi::GetLastError() };
-            println!("could not get thread context, error is: {}", err);
-            return Err(());
-        }
-
-        context.0.Eip = start_addr;
-        let context_result = unsafe {
-            winbase::Wow64SetThreadContext(thread_handle, &mut context.0)
-        };
-
-        if context_result == 0 {
-            let err = unsafe { errhandlingapi::GetLastError() };
-            println!("could not set thread context, error is: {}", err);
-            return Err(());
-        }
-
-        let resume_result = unsafe {
-            processthreadsapi::ResumeThread(thread_handle)
-        };
-
-        if resume_result == 0xFFFFFFFF {
-            let err = unsafe { errhandlingapi::GetLastError() };
-            println!("thread could not resume, error is: {}", err);
-            return Err(());
-        }  
-
-        std::thread::sleep(Duration::from_millis(1000));
-
-        let mut thread_exit_code = 0;
-        let exit_code_result = unsafe {
-            processthreadsapi::GetExitCodeThread(thread_handle, &mut thread_exit_code)
-        };
-
-        if exit_code_result == 0 {
-            let err = unsafe { errhandlingapi::GetLastError() };
-            println!("could not get exit code, error is: {}", err);
-            return Err(());
-        }
-        println!("thread exit code is: {}", thread_exit_code);
-
-        Ok(())
+    if thread_handle.is_null() {
+        let err = unsafe { errhandlingapi::GetLastError() }; 
+        println!("could not create thread, error is: {}", err);
+        return Err(());
     }
-    */
+
+    println!("created thread, id is: {}", thread_id);
+
+    let mut context: AlignedContext = unsafe { mem::zeroed() };
+    context.0.ContextFlags = WOW64_CONTEXT_FULL;
+    let context_result = unsafe {
+        winbase::Wow64GetThreadContext(thread_handle, &mut context.0)
+    };
+    
+    if context_result == 0 {
+        let err = unsafe { errhandlingapi::GetLastError() };
+        println!("could not get thread context, error is: {}", err);
+        return Err(());
+    }
+
+    context.0.Eip = start_addr;
+    let context_result = unsafe {
+        winbase::Wow64SetThreadContext(thread_handle, &mut context.0)
+    };
+
+    if context_result == 0 {
+        let err = unsafe { errhandlingapi::GetLastError() };
+        println!("could not set thread context, error is: {}", err);
+        return Err(());
+    }
+
+    let resume_result = unsafe {
+        processthreadsapi::ResumeThread(thread_handle)
+    };
+
+    if resume_result == 0xFFFFFFFF {
+        let err = unsafe { errhandlingapi::GetLastError() };
+        println!("thread could not resume, error is: {}", err);
+        return Err(());
+    }  
+
+    std::thread::sleep(Duration::from_millis(1000));
+
+    let mut thread_exit_code = 0;
+    let exit_code_result = unsafe {
+        processthreadsapi::GetExitCodeThread(thread_handle, &mut thread_exit_code)
+    };
+
+    if exit_code_result == 0 {
+        let err = unsafe { errhandlingapi::GetLastError() };
+        println!("could not get exit code, error is: {}", err);
+        return Err(());
+    }
+    println!("thread exit code is: {}", thread_exit_code);
+
+    Ok(())
+}
+*/
