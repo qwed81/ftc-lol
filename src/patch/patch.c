125ab7c3485d27109ec693344aaceb7f90caf0f8
Joshua Cox <jcox.12.2002@gmail.com>
2023-03-15 13:46:50 -0700

    added memory searching

--- src/patch/patch.c
+++ src/patch/patch.c
@@ -12,6 +12,7 @@ static uint32_t str_len(const char* str, uint32_t max);
 static bool str_cmp(const char* a, const char* b);
 static void* apply_jump_fn_hook(void* new_func, void* addr);
 static void apply_swap_return_hook();
+static void* find_mem_pattern(void* begin, uint32_t len, const char* pattern_str);
 
 __attribute__((fastcall))
 void log_int(uint32_t val);
@@ -69,18 +70,36 @@ void init(void* _kernel32_addr) {
     post_hook_ReadFile = (ReadFileType) apply_jump_fn_hook(my_ReadFile, ReadFileAddr);
     post_hook_CreateFileA = (CreateFileAType) apply_jump_fn_hook(my_CreateFileA, CreateFileAAddr); 
     post_hook_CreateFileW = (CreateFileWType) apply_jump_fn_hook(my_CreateFileW, CreateFileWAddr); 
-    apply_swap_return_hook();
-
-    log_str("\nall is well \n");
+    // apply_swap_return_hook();
 }
 
+static const char* filter_expected_return = "68 A0 02 00 00 E8 ?? ?? ?? 83 C4 50";
+static const char* filter_fn = "74 02 FF E0 8B 44 24 04 85 C0 75 3E";
+
 static void apply_swap_return_hook() {
+    // void* er = find_mem_pattern((void*)0x0, 0xFFFFFFFF, filter_expected_return);
+    void* fn = find_mem_pattern((void*)0xb70000, 0x0FFFFFFF, filter_fn);
+
+    /*
+    fn_ptr_addr = find_mem_pattern((void*)0x0, 0xFFFFFFFF, "");
     fn_ptr_addr = (void**)0x0243F368;
     original_jump_addr = (void*)0x01939EC2;
     expected_return_addr = (void*)0x014fefe1;
 
     void* actual_swap_return = resolve_runtime_addr(swap_return);
     *fn_ptr_addr = actual_swap_return;
+    log_str("\nfn_ptr_addr: ");
+    log_int((size_t)fn_ptr_addr);
+    log_str("\noriginal_jump_addr: ");
+    log_int((size_t)original_jump_addr);
+    log_str("\expected_return_addr: ");
+    log_int((size_t)expected_return_addr);
+    */
+
+    log_str("\ner: ");
+    // log_int((size_t)er);
+    log_str("\nfn");
+    log_int((size_t)fn);
 } 
 
 static void* file_handle = NULL; 
@@ -97,12 +116,14 @@ static void* my_CreateFileA(const char* name, uint32_t access, uint32_t share, v
     log_str("\ncreate file A: ");
     log_str(name);
 
+    /*
     if (str_cmp(name, "DATA/FINAL/Champions/Nunu.wad.client")) {
         name = "C:/Users/josh/Desktop/cslol-manager/profiles/Default Profile/DATA/FINAL/Champions/Nunu.wad.client";
     } 
     else if (str_cmp(name, "DATA/FINAL/UI.wad.client")) {
         name = "C:/Users/josh/Desktop/cslol-manager/profiles/Default Profile/DATA/FINAL/UI.wad.client";
     }
+    */
 
     log_str("\ncall count is: ");
     log_int(call_count);
@@ -146,6 +167,159 @@ static void* apply_jump_fn_hook(void* new_func, void* addr) {
     }
 
     return new_addr;
+}
+
+static int32_t int_from_hex_char(char c) {
+    if (c >= 'A' && c <= 'F') {
+        return c - 'A' + 10;
+    }
+    else if (c >= 'a' && c <= 'f') {
+        return c - 'a' + 10;
+    }
+    else if (c >= '0' && c <= '9') {
+        return c - '0';
+    }
+    else if (c == '?') {
+        return -1;
+    }
+
+    return -2;
+}
+
+static bool can_read_page(void* addr) {
+    char buf;
+    uint32_t can_read = ReadProcessMemory(CURRENT_PROCESS, addr, &buf, 1, NULL);
+    return can_read != 0;
+}
+
+// returns the beginning of the memory pattern, or NULL if not found in the length
+// the memory pattern must be in the format of "AA ?? F0" null terminated, and must contain
+// full bytes
+static void* find_mem_pattern(void* start, uint32_t len, const char* pattern_str) {
+    const uint32_t max_pattern_bytes = 70;
+    // should always be less than max_pattern_bytes * 3 so it does not overflow buffer
+    const uint32_t max_pattern_str = max_pattern_bytes * 3 - 3;
+
+    uint32_t actual_str_len = str_len(pattern_str, max_pattern_str);
+    // greater than or equal to the max, should fail
+    // the string is too long to find
+    if (actual_str_len == max_pattern_str || actual_str_len < 2) {
+        return (void*)-1;
+    }
+
+    uint32_t pattern_len = (actual_str_len + 1) / 3;
+    bool skip[max_pattern_bytes];
+    char required_val[max_pattern_bytes];
+
+    for (uint32_t i = 0; i < actual_str_len; i += 3) {
+        int32_t a = int_from_hex_char(pattern_str[i]);
+        int32_t b = int_from_hex_char(pattern_str[i + 1]);
+        int32_t byte_index = i / 3;
+
+        // one of the characters is invalid
+        if (a == -2 || b == -2) {
+            return (void*)-1;
+        }
+        // one of them is a question mark but both of them aren't
+        if ((a == -1 || b == -1) && a != b) {
+            return (void*)-1;
+        }
+
+        // they are both ??, we can say to skip this one
+        if (a == -1) {
+            skip[byte_index] = true;
+        }
+        else {
+            skip[byte_index] = false;
+        }
+
+        // if they should be skipped it doesn't matter what this field is
+        // otherwise figure out what the byte should be
+        required_val[byte_index] = (a << 4) | b;
+    }
+
+    // can not have trailing skips
+    if (skip[pattern_len - 1]) {
+        return (void*)-1;
+    }
+
+    const uint32_t page_size = 4096;
+    char* base = (char*)((size_t)start / 4096);
+
+    uint64_t index;
+    bool can_read_next;
+    if (can_read_page(base)) {
+        index = (size_t)start % 4096;
+        can_read_next = can_read_page(start + page_size);
+    }
+    else {
+        // setup so the start of the loop will 
+        // know if it can read
+        index = page_size;
+        can_read_next = can_read_page(start + page_size);
+    }
+
+    while (index < (uint64_t)len) {
+        // on entering a new page boundary
+        if (index % page_size == 0) {
+            // if can read next, don't need to do anything but set
+            // if we can read the next page again
+
+            // if can't that means this page can't be read, because we are switching
+            // to "next_page". so keep
+            // checking until we can read the page
+            if (can_read_next == false) {
+                bool can_read = false;
+                while (can_read == false) {
+                    index += page_size;
+                    if (index >= len) {
+                        return NULL;
+                    }
+                    can_read = can_read_page(base + index);
+                }
+            }
+
+            // now that we can read the page, we need to set if we
+            // can read the next one
+            can_read_next = can_read_page(base + index + page_size);
+        }
+
+        bool found = true;
+        for (uint32_t j = 0; j < pattern_len; j += 1) {
+            if (skip[j]) {
+                continue;
+            }
+
+            // because we are searching in our own address space, it is
+            // possible the value gets matched against itself if it is in the range
+            if (base + index == required_val) {
+                found = false;
+                break;
+            }
+
+            // because we can't read the next page, we know that this pattern can not
+            // be satisfied because it does not fit in the remaining amount to be read
+            if (can_read_next == false && pattern_len + index % page_size >= page_size) {
+                found = false;
+                break;
+            }
+
+            if (required_val[j] != base[index + j]) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) {
+            log_str("\nindex: ");
+            log_int(index);
+            return base + index;
+        }
+
+        index += 1;
+    }
+
+    return NULL;
 }
 
 bool str_cmp(const char* a, const char* b) {
--- src/patch/system.h
+++ src/patch/system.h
@@ -7,6 +7,7 @@ typedef uint16_t WCHAR;
 typedef void* (__stdcall *CreateFileAType)(const char*, uint32_t, uint32_t, void*, uint32_t, uint32_t, void*); 
 typedef void* (__stdcall *CreateFileWType)(const WCHAR*, uint32_t, uint32_t, void*, uint32_t, uint32_t, void*); 
 typedef uint32_t (__stdcall *ReadFileType)(void*, void*, uint32_t, uint32_t*, uint32_t*);
+typedef uint32_t (__stdcall *ReadProcessMemoryType)(void*, const void*, void*, uint32_t, uint32_t*);
 
 typedef uint32_t (__stdcall *WriteFileType)(void*, const void*, uint32_t, uint32_t*, void*);
 typedef uint32_t (__stdcall *VirtualProtectType)(void*, uint32_t, uint32_t, uint32_t*);
@@ -23,6 +24,7 @@ extern void* kernel32_addr;
 #define PAGE_EXECUTE_READ 0x20
 #define PAGE_EXECUTE_READWRITE 0x40
 #define PAGE_READWRITE 0x04
+#define CURRENT_PROCESS ((void*)(-1))
 
 #define CreateFileWAddr (kernel32_addr + 0x23810)
 #define CreateFileAAddr (kernel32_addr + 0x23800)
@@ -30,8 +32,10 @@ extern void* kernel32_addr;
 #define VirtualProtectAddr (kernel32_addr + 0x20B90)
 #define GetLastErrorAddr (kernel32_addr + 0x1E640)
 #define ReadFileAddr (kernel32_addr + 0x23B90)
+#define ReadProcessMemoryAddr (kernel32_addr + 0x35830)
 
 #define pre_hook_CreateFileA ((CreateFileAType)CreateFileAAddr)
 #define WriteFile ((WriteFileType)WriteFileAddr)
 #define VirtualProtect ((VirtualProtectType)VirtualProtectAddr)
 #define GetLastError ((GetLastErrorType)GetLastErrorAddr)
+#define ReadProcessMemory ((ReadProcessMemoryType)ReadProcessMemoryAddr)
