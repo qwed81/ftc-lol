02dbb178ecbce8c6283acf902bc45f89a3d02d06
Joshua Cox <jcox.12.2002@gmail.com>
2023-04-26 18:04:23 -0700

    build to target directory

--- make_release.py
+++ make_release.py
@@ -3,22 +3,22 @@ import shutil
 import subprocess
 import sys
 
-if os.path.exists("./release-min"):
-    shutil.rmtree("./release-min")
+if os.path.exists("./target/release-min"):
+    shutil.rmtree("./target/release-min")
 
-if os.path.exists("./release-full"):
-    shutil.rmtree("./release-full")
+if os.path.exists("./target/release-full"):
+    shutil.rmtree("./target/release-full")
 
-os.mkdir("./release-min")
-os.mkdir("./release-full")
+os.mkdir("./target/release-min")
+os.mkdir("./target/release-full")
 
 subprocess.run(["cargo", "build", "--release"])
 
 for file in ["patch", "segment_maker.exe", "server_cli.exe", "client_cli.exe"]:
-    shutil.copyfile("./target/release/" + file, "./release-full/" + file)
+    shutil.copyfile("./target/release/" + file, "./target/release-full/" + file)
 
 for file in ["patch", "client_cli.exe"]:
-    shutil.copyfile("./target/release/" + file, "./release-min/" + file)
+    shutil.copyfile("./target/release/" + file, "./target/release-min/" + file)
 
 # plan to remove in the future when proper support is implemented
 if len(sys.argv) < 2:
@@ -31,10 +31,10 @@ if name != "mod-tools.exe":
     print(f"file {name} is not mod-tools")
     exit(1)
 
-shutil.copy(lol_tools_path, "./release-full/mod-tools.exe")
+shutil.copy(lol_tools_path, "./target/release-full/mod-tools.exe")
 
-os.mkdir("./release-min/client_packages")
+os.mkdir("./target/release-min/client_packages")
 
-os.mkdir("./release-full/client_packages")
-os.mkdir("./release-full/server_packages")
+os.mkdir("./target/release-full/client_packages")
+os.mkdir("./target/release-full/server_packages")
 
